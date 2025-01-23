591f64ad03c047152cdf65d1423c89d44c292d46
Phoenix <aadilsiddiqui2016+github@gmail.com>
2025-01-23 12:54:37 +0530

    Cart fucnctionality added

---
+++ app/cart/page.js
@@ -0,0 +1,12 @@
+"use client"
+import React from 'react';
+import Cart from '../../components/Cart'
+
+export default function Cartpage (){
+  return (
+    <div>
+      <Cart />
+    </div>
+  );
+};
+
